39a7dcdf0e423e547950fcc27513cf766e2e6695
Anderson M <78233612+fm-anderson@users.noreply.github.com>
2024-04-23 12:05:28 -0400

    fix: static data moved to json files

--- src/utils/seasons.js
+++ src/utils/seasons.js
@@ -1,50 +1,11 @@
-export const seasons = [
-  {
-    key: "spring",
-    name: "Spring Equinox",
-    image: "/images/seasons/spring.webp",
-    dates: {
-      2024: new Date("2024-03-20T15:58:00Z"),
-      2025: new Date("2025-03-20T21:45:00Z"),
-      2026: new Date("2026-03-20T03:25:00Z"),
-      2027: new Date("2027-03-20T09:24:00Z"),
-      2028: new Date("2028-03-20T15:17:00Z"),
-    },
-  },
-  {
-    key: "summer",
-    name: "Summer Solstice",
-    image: "/images/seasons/summer.webp",
-    dates: {
-      2024: new Date("2024-06-20T22:51:00Z"),
-      2025: new Date("2025-06-21T04:42:00Z"),
-      2026: new Date("2026-06-21T10:24:00Z"),
-      2027: new Date("2027-06-21T16:10:00Z"),
-      2028: new Date("2028-06-20T22:02:00Z"),
-    },
-  },
-  {
-    key: "fall",
-    name: "Autumn Equinox",
-    image: "/images/seasons/fall.webp",
-    dates: {
-      2024: new Date("2024-09-22T19:02:00Z"),
-      2025: new Date("2025-09-23T00:19:00Z"),
-      2026: new Date("2026-09-23T06:05:00Z"),
-      2027: new Date("2027-09-23T11:50:00Z"),
-      2028: new Date("2028-09-22T17:44:00Z"),
-    },
-  },
-  {
-    key: "winter",
-    name: "Winter Solstice",
-    image: "/images/seasons/winter.webp",
-    dates: {
-      2024: new Date("2024-12-21T17:20:00Z"),
-      2025: new Date("2025-12-21T23:03:00Z"),
-      2026: new Date("2026-12-22T04:50:00Z"),
-      2027: new Date("2027-12-22T10:42:00Z"),
-      2028: new Date("2028-12-21T16:19:00Z"),
-    },
-  },
-];
+import seasonsData from "./db/seasons.json";
+
+const seasons = seasonsData.map((season) => {
+  const formattedDates = {};
+  for (const year in season.dates) {
+    formattedDates[year] = new Date(season.dates[year]);
+  }
+  return { ...season, dates: formattedDates };
+});
+
+export default seasons;
